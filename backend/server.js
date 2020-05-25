37be69ccaa37a8cf406e4ac3e63c8edd1f600563
Lune <yari.lune@gmail.com>
2020-05-25 13:51:07 +0200

    fix problem with cors for nodemon and add db connection to server file

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
 
 require('dotenv').config(); // to have variables in dotenv file
 
@@ -8,6 +10,21 @@ const port = process.env.PORT || 5000; // specify the port
 
 app.use(cors()); // app to use cors
 app.use(express.json()); // allow to parse json
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri,
+    { 
+        useNewUrlParser : true, 
+        useCreateIndex: true
+    }
+);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MONGODB database connection established')
+});
+
+
 
 // listen to the server(start server on port)
 app.listen(port,() => {
