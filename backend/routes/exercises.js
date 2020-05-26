d9707b4b424e4d937b9016e73a536be97764d802
Lune <yari.lune@gmail.com>
2020-05-26 09:48:45 +0200

    comment last commit changes

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -30,29 +30,30 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err)); // if error return 400 with err message
 });
 
-// get the exercise with the given id
+// get the exercise with the given id(request get)
 router.route('/:id').get((req, res) => {
     Exercise.findById(req.params.id)
         .then(exercise => res.json(exercise))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-// delete the exercise with the given id
+// delete the exercise with the given id(request delete)
 router.route('/:id').delete((req, res) => {
     Exercise.findByIdAndDelete(req.params.id)
         .then(() => res.json('Exercise deleted.'))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-// update exercise with the given id
+// update exercise with the given id(request post)
 router.route('/update/:id').post((req, res) => {
     Exercise.findById(req.params.id)
-        .then(exercise => {
+        .then(exercise => { // if found replace db data for given positions with the new data
             exercise.username = req.body.username;
             exercise.description = req.body.description;
             exercise.duration = Number(req.body.duration);
             exercise.date = Date.parse(req.body.date);
 
+            // save new data to db
             exercise.save()
                 .then(() => res.json('Exercise updated!'))
                 .catch(err => res.status(400).json('Error: ' + err));
